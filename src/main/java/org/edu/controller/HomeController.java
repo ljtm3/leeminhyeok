ded65aa97e2a5be895c9a5499d44c6a2e9f508e4
dlals <dlals@DESKTOP-U6R14A4>
2020-11-16 11:47:06 +0900

    202011163교시

--- src/main/java/org/edu/controller/HomeController.java
+++ src/main/java/org/edu/controller/HomeController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
+ * 앱을 위한 홈페이지 요청(request)을 처리한다(아래).
+ * http://localhost:8080/ 요청을 받았을때, 변수를 만들어서 화면과 뿌러준다.
  */
 @Controller
 public class HomeController {
@@ -24,8 +25,8 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		logger.info("환영합니다. 현재 여러분 컴퓨터언어는 {} 입니다.", locale);
+		logger.debug("환영합니다. 현재 여러분의 컴퓨터 언어는" + locale + " 입니다..");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
